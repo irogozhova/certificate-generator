0e40ca01e1edd48a3c13d4ba041e4f7502766227
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2018-09-24 21:46:05 +0300

    'more'

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -7,6 +7,40 @@ let CertificateForm = props => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Choose your certificate style</legend>
+        <div>
+          <h2>Orientation:</h2>
+          <div>
+            <label><Field name="orientation" component="input" type="radio" value="landscape" /> Landscape</label>
+            <label><Field name="orientation" component="input" type="radio" value="portrait" /> Portrait</label>
+          </div>
+        </div>
+        <div>
+          <h2>Background color:</h2>
+          <label>
+            <Field name="bgcolor" component="input" type="color" value="#4286f4" />
+            Choose color
+          </label>
+        </div>
+      <div>
+          <h2>Font:</h2>
+          <Field name="font" component="select">
+            <option></option>
+            <option value="Times New Roman">Times New Roman</option>
+            <option value="Verdana">Verdana</option>
+            <option value="Comic Sans">Comic Sans</option>
+          </Field>
+        </div>
+        {/* <div>
+          <h2>Decorations:</h2>
+          <Checkboxes 
+            options={decorOptions}
+            selectedOptions={decor}
+            handleChange={this.handleDecorCheckbox}
+          />
+        </div> */}
+      </fieldset>
       <div>
         <label htmlFor="firstName">First Name</label>
         <Field name="firstName" component="input" type="text" />
@@ -17,8 +51,8 @@ let CertificateForm = props => {
       </div>
       <button type="submit">Submit</button>
     </form>
-  )
-}
+  );
+};
 
 CertificateForm = reduxForm({
   form: 'certificate'
--- src/containers/Preview/Preview.js
+++ src/containers/Preview/Preview.js
@@ -7,6 +7,9 @@ class Preview extends Component {
   render() {
 
     const { values } = this.props;
+
+    const orientation = (values != null && values.orientation !== undefined) ? values.orientation : "(orientation)";
+    const bgcolor = (values != null && values.bgcolor !== undefined) ? values.bgcolor : "(bgcolor)";
     const firstName = (values != null && values.firstName !== undefined) ? values.firstName : "(First name)";
     const lastName = (values != null && values.lastName !== undefined) ? values.lastName : "(Last name)";
 
@@ -25,7 +28,9 @@ class Preview extends Component {
           <div className={styles.credentials}>
             <div className={styles.tableColumn}>
               <h3>Date:</h3>
+              <div>{orientation}</div>
               <h3>Place:</h3>
+              <div>{bgcolor}</div>
             </div>
             <div className={styles.tableColumn}>
               <h3>Name:</h3>
